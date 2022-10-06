d0bbb5eaaa4981ee2cec87003c1c51938a03ef38
Srashti Jain <srashti0204@gmail.com>
2022-10-07 03:08:58 +0530

    feat: added grid to show data from json file

--- index.js
+++
@@ -1,8 +0,0 @@
-function getData() {
-    fetch("./data.json")
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-        })
-        .catch(err => console.log(err));
-}
